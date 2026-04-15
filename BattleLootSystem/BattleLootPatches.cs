227595c2f210d3901e9859ef7ac8c0cb4b802322
Shtoper26 <Shtoper@gmail.com>
2026-04-15 19:46:26 +0300

    Исправли ошибку дублирования выдачи лута победителю , при участии более 2 отрядов, исправили дублирование выдачи выданного своим Юнитам лута для победителя, отключи внильную выдачу лута.

--- BattleLootSystem/BattleLootPatches.cs
+++ BattleLootSystem/BattleLootPatches.cs
@@ -1,14 +1,31 @@
 ﻿using HarmonyLib;
 using TaleWorlds.CampaignSystem.MapEvents;
 using TaleWorlds.CampaignSystem.Party;
+using TaleWorlds.CampaignSystem.Roster;
 using TaleWorlds.Core;
+using System.Collections.Generic;
+using System.Linq;
+using System;
 
 namespace RpgMod1.BattleLootSystem
 {
+    // Патч для блокировки ванильной генерации лута
+    [HarmonyPatch(typeof(MapEvent))]
+    public class VanillaLootBlocker
+    {
+        [HarmonyPrefix]
+        [HarmonyPatch("LootDefeatedPartyItems")]
+        [HarmonyPatch("LootDefeatedPartyCasualties")]
+        static bool Prefix()
+        {
+            // Возвращаем false, чтобы оригинальные методы игры НЕ выполнялись
+            return false;
+        }
+    }
+
     [HarmonyPatch(typeof(MapEvent), "OnBattleWon")]
     public class BattleLootPatches
     {
-        // Оставляем только ОДИН метод Postfix
         [HarmonyPostfix]
         public static void Postfix(MapEvent __instance)
         {
@@ -17,35 +34,85 @@ namespace RpgMod1.BattleLootSystem
             var winnerSide = __instance.GetMapEventSide(__instance.WinningSide);
             var loserSide = __instance.GetMapEventSide(__instance.WinningSide.GetOppositeSide());
 
-            PartyBase mainWinner = winnerSide.LeaderParty;
-            if (mainWinner == null) return;
-
-            // 1. Возврат своим
+            // 1. СОБСТВЕННЫЙ ВОЗВРАТ (Победители забирают своё)
             foreach (var winnerPartyInfo in winnerSide.Parties)
             {
-                var issuedRoster = BattleEquipmentTracker.GetIssuedRoster(__instance, winnerPartyInfo.Party.Id.ToString());
-                if (issuedRoster != null)
+                var issued = BattleEquipmentTracker.GetIssuedRoster(__instance, winnerPartyInfo.Party.Id.ToString());
+                if (issued != null && issued.Count > 0)
                 {
-                    mainWinner.ItemRoster.Add(issuedRoster);
+                    winnerPartyInfo.Party.ItemRoster.Add(issued);
                 }
             }
 
-            // 2. Сбор с проигравших
+            // 2. СБОР ОБЩЕГО КОТЛА ТРОФЕЕВ
+            ItemRoster totalLootContainer = new ItemRoster();
             foreach (var loserPartyInfo in loserSide.Parties)
             {
-                var issuedRoster = BattleEquipmentTracker.GetIssuedRoster(__instance, loserPartyInfo.Party.Id.ToString());
-                if (issuedRoster != null)
-                {
-                    mainWinner.ItemRoster.Add(issuedRoster);
-                }
+                // Забираем элитку из трекера
+                var issuedToLoser = BattleEquipmentTracker.GetIssuedRoster(__instance, loserPartyInfo.Party.Id.ToString());
+                if (issuedToLoser != null) totalLootContainer.Add(issuedToLoser);
 
-                if (loserPartyInfo.Party.ItemRoster != null && loserPartyInfo.Party.ItemRoster.Count > 0)
+                // Забираем всё из инвентаря (коровы, зерно, шмотки)
+                if (loserPartyInfo.Party.ItemRoster != null)
                 {
-                    mainWinner.ItemRoster.Add(loserPartyInfo.Party.ItemRoster);
+                    totalLootContainer.Add(loserPartyInfo.Party.ItemRoster);
                     loserPartyInfo.Party.ItemRoster.Clear();
                 }
             }
 
+            if (totalLootContainer.Count == 0)
+            {
+                BattleEquipmentTracker.ClearBattleData(__instance);
+                return;
+            }
+
+            // 3. РАСЧЕТ ДОЛЕЙ (По количеству "мяса")
+            int totalWinnerTroops = winnerSide.Parties.Sum(p => p.Party.NumberOfAllMembers);
+            if (totalWinnerTroops <= 0) return;
+
+            // Сортируем победителей по убыванию силы, чтобы остатки (округление) забирал сильнейший
+            var sortedWinners = winnerSide.Parties
+                .OrderByDescending(p => p.Party.NumberOfAllMembers)
+                .ToList();
+
+            // 4. ДЕЛЕЖКА ПИРОГА
+            // Перебираем каждый тип предмета в общем котле
+            for (int i = 0; i < totalLootContainer.Count; i++)
+            {
+                ItemRosterElement element = totalLootContainer.GetElementCopyAtIndex(i);
+                int remainingAmount = element.Amount;
+
+                foreach (var winnerInfo in sortedWinners)
+                {
+                    if (remainingAmount <= 0) break;
+
+                    // Считаем долю: (Кол-во предметов * Юниты отряда) / Всего юнитов
+                    // Используем double для точности, затем округляем
+                    double share = (double)element.Amount * winnerInfo.Party.NumberOfAllMembers / totalWinnerTroops;
+                    int amountToGive = (int)Math.Round(share);
+
+                    // Если это последний (самый крупный) или единственный отряд, отдаем остаток
+                    if (winnerInfo == sortedWinners.Last() || amountToGive > remainingAmount)
+                    {
+                        amountToGive = remainingAmount;
+                    }
+
+                    if (amountToGive > 0)
+                    {
+                        winnerInfo.Party.ItemRoster.AddToCounts(element.EquipmentElement, amountToGive);
+                        remainingAmount -= amountToGive;
+                    }
+                }
+
+                // Если после округлений что-то осталось (например, 1 меч на 3 равных отряда), 
+                // отдаем самому крупному (он первый в списке)
+                if (remainingAmount > 0)
+                {
+                    sortedWinners[0].Party.ItemRoster.AddToCounts(element.EquipmentElement, remainingAmount);
+                }
+            }
+
+            // 5. ОЧИСТКА
             BattleEquipmentTracker.ClearBattleData(__instance);
         }
     }
